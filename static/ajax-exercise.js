debc6f0c06e2f284ce06da0e5366515c0f310393
Yichi Guo <yichisa@gmail.com>
2021-09-22 16:55:50 -0700

    completed ajax exercise

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -37,6 +37,25 @@ function orderMelons(evt) {
   evt.preventDefault();
 
   // TODO: show the result message after your form
+  // const formInputs = {
+  //   melonType: $('#qty-field').val(),
+  //   quantity: $('#melon-type-field').val()
+  // };
+    const formInputs = {
+      melon_type: $('#melon-type-field').val(),
+      qty: $('#qty-field').val(),
+    };
+    
+    $.post('/order-melons.json', formInputs, resultInfo => {
+      if (resultInfo['code'] === 'ERROR') {
+        $('#order-status').addClass('order-error');
+      } else if (resultInfo['code'] === 'OK') {
+        $('#order-status').removeClass('order-error');
+      }  
+      $('#order-status').text(`${resultInfo['msg']}`);
+
+    })
+
   // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
 }
 
